b53b9a1324022e069377cd99e452f22c4bdaa1bb
Wolfram Rösler <wolfram@roesler-ac.de>
2017-04-08 15:06:34 +0200

    Allow a file with exclusion patterns in the target dir

--- cloud-backup.sh
+++ cloud-backup.sh
@@ -4,17 +4,24 @@
 BASE=~/cloud-backup
 
 for user in linus;do
-
 	echo "Backing up $user"
 
+	# Make the destination directory
 	DIR=$BASE/$user
 	mkdir -p $DIR || exit
 
+	# Mount the WebDAV directory (umount first in case it's already mounted)
 	umount /mnt/$user &>/dev/null
 	mount /mnt/$user || exit
 
-	rdiff-backup --terminal-verbosity 5 /mnt/$user $DIR || exit
+	# Find the file with exclude patterns
+	EXCLUDE=$DIR/exclude
+	[ -f $EXCLUDE ] || EXCLUDE=/dev/null
+
+	# Do it
+	rdiff-backup --exclude-filelist $EXCLUDE --terminal-verbosity 5 /mnt/$user $DIR || exit
 	
+	# Unmount the WebDAV directory
 	umount /mnt/$user
 
 	echo
