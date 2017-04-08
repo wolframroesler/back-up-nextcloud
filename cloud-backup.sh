1d07051888865ffe33198671e3d0375cd488ead2
Wolfram Rösler <wolfram@roesler-ac.de>
2017-04-08 13:27:47 +0200

    Implement first prototype version

--- cloud-backup.sh
+++ cloud-backup.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+# Backups go here
+BASE=~/cloud-backup
+
+for user in linus;do
+
+	echo "Backing up $user"
+
+	DIR=$BASE/$user
+	mkdir -p $DIR || exit
+
+	mount /mnt/$user || exit
+
+	rdiff-backup --terminal-verbosity 9 /mnt/$user $DIR || exit
+	
+	umount /mnt/$user
+done
