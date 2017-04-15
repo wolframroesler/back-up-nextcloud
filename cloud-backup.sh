0df1e08f544e29404036262cd3f1265fed0ed4c3
Wolfram Rösler <wolfram@roesler-ac.de>
2017-04-15 15:17:56 +0200

    Process accounts in random order, and reduce log verbosity

--- cloud-backup.sh
+++ cloud-backup.sh
@@ -5,8 +5,11 @@
 # Backups go here
 BASE=~/cloud-backup
 
-# Get user list from fstab file
-grep webdav /etc/fstab | cut -f2 -d' ' | cut -f3 -d/ | sort | while read user;do
+# Get user list from fstab file. Process users in a different order each time
+# to have equal probability of one user not being backed up because of another
+# user failing. Remember that a failed backup can lead to a very lengthy
+# "regression" (rdiff-backup fetching all files anew from the cloud).
+grep webdav /etc/fstab | cut -f2 -d' ' | cut -f3 -d/ | shuf | while read user;do
 
 	# Show the starting message
 	echo
@@ -21,8 +24,9 @@ grep webdav /etc/fstab | cut -f2 -d' ' | cut -f3 -d/ | sort | while read user;do
 	umount /mnt/$user &>/dev/null
 	mount /mnt/$user || exit
 
-	# Now do the actual backup
-	rdiff-backup --force --exclude-filelist $(dirname $0)/exclude.txt --verbosity 7 --terminal-verbosity 3 /mnt/$user $DIR || exit
+	# Now do the actual backup. Some log output goes to stdout, more
+	# detailed logs go to $DIR/rdiff-backup-data/backup.log.
+	rdiff-backup --exclude-filelist $(dirname $0)/exclude.txt --verbosity 6 --terminal-verbosity 3 /mnt/$user $DIR || exit
 	
 	# Unmount the WebDAV directory
 	umount /mnt/$user
