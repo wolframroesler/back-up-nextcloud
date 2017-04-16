211f24b0e842b518196781b5ea4c4199f8e834fc
Wolfram Rösler <wolfram@roesler-ac.de>
2017-04-16 13:08:53 +0200

    Use plain old rsync instead of rdiff-backup

--- cloud-backup.sh
+++ cloud-backup.sh
@@ -4,11 +4,11 @@
 
 # Backups go here
 BASE=~/cloud-backup
+mkdir -p $BASE || exit
 
 # Get user list from fstab file. Process users in a different order each time
 # to have equal probability of one user not being backed up because of another
-# user failing. Remember that a failed backup can lead to a very lengthy
-# "regression" (rdiff-backup fetching all files anew from the cloud).
+# user failing.
 grep webdav /etc/fstab | cut -f2 -d' ' | cut -f3 -d/ | shuf | while read user;do
 
 	# Show the starting message
@@ -16,17 +16,12 @@ grep webdav /etc/fstab | cut -f2 -d' ' | cut -f3 -d/ | shuf | while read user;do
 	echo "$(date) Backing up $user"
 	echo
 
-	# Make the destination directory
-	DIR=$BASE/$user
-	mkdir -p $DIR || exit
-
 	# Mount the WebDAV directory (umount first in case it's already mounted)
 	umount /mnt/$user &>/dev/null
 	mount /mnt/$user || exit
 
-	# Now do the actual backup. Some log output goes to stdout, more
-	# detailed logs go to $DIR/rdiff-backup-data/backup.log.
-	rdiff-backup --exclude-filelist $(dirname $0)/exclude.txt --verbosity 6 --terminal-verbosity 3 /mnt/$user $DIR || exit
+	# Now do the actual backup
+	rsync -a -vv --exclude-from="$(dirname $0)/exclude.txt" /mnt/$user $BASE || exit
 	
 	# Unmount the WebDAV directory
 	umount /mnt/$user
