d9cc4815585827559a3c3126e904785cc16659fb
Wolfram Rösler <wolfram@roesler-ac.de>
2017-04-16 18:49:13 +0200

    Improve logging

--- cloud-backup.sh
+++ cloud-backup.sh
@@ -6,10 +6,12 @@
 BASE=~/cloud-backup
 mkdir -p $BASE || exit
 
-# Get user list from fstab file. Process users in a different order each time
-# to have equal probability of one user not being backed up because of another
-# user failing.
-grep webdav /etc/fstab | cut -f2 -d' ' | cut -f3 -d/ | shuf | while read user;do
+# rsync log file
+LOG=/tmp/cloud-backup.log
+rm -f $LOG
+
+# Get user list from fstab file
+grep webdav /etc/fstab | cut -f2 -d' ' | cut -f3 -d/ | sort | while read user;do
 
 	# Show the starting message
 	echo
@@ -21,7 +23,7 @@ grep webdav /etc/fstab | cut -f2 -d' ' | cut -f3 -d/ | shuf | while read user;do
 	mount /mnt/$user || exit
 
 	# Now do the actual backup
-	rsync -a -vv --exclude-from="$(dirname $0)/exclude.txt" /mnt/$user $BASE || exit
+	rsync -a -v --exclude-from="$(dirname $0)/exclude.txt" /mnt/$user $BASE --log-file=$LOG --partial --progress --stats || exit
 	
 	# Unmount the WebDAV directory
 	umount /mnt/$user
